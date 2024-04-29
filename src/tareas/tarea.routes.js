224c98b9a82b740257584e93a4c7826b3d70e7da
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-04-29 10:16:25 -0600

    Added put route to tarea.routes.js

--- src/tareas/tarea.routes.js
+++ src/tareas/tarea.routes.js
@@ -5,19 +5,28 @@ import { validarCampos } from "../middlewares/validar-campos.js";
 
 const router = Router()
 
-router.get("/verTareas", tareasGet);
+router.get( "/verTareas", tareasGet );
 
 router.post(
     "/agregar", [
-    check("nombreTarea", "El nombre de la tarea es obligatoria").not().isEmpty(),
-    check("description", "La descripcion es obligatoria").not().isEmpty(),
-    check("fechaDeInicio", "La fecha de inicio es obligatoria").not().isEmpty(),
-    check("fechaDeCierre", "La fecha de cierre es obligatoria").not().isEmpty(),
-    check("name", "El nombre es obligatorio").not().isEmpty(),
-    check("lastName", "El apellido es obligatorio").not().isEmpty(),
+    check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
+    check( "description", "La descripcion es obligatoria" ).not().isEmpty(),
+    check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
+    check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
+    check( "name", "El nombre es obligatorio" ).not().isEmpty(),
+    check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
     validarCampos
 ],
     tareasPost
-)
+);
+
+router.put( '/actualizar/:id', [check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
+check( "description", "La descripcion es obligatoria" ).not().isEmpty(),
+check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
+check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
+check( "estado", "El estado es obligatorio" ).not().isEmpty(),
+check( "name", "El nombre es obligatorio" ).not().isEmpty(),
+check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
+    validarCampos], tareasPut );
 
 export default router;
