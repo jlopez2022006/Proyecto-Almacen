4d5aef8300881db4441c0f7fb65a71bdc22b6b16
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-04-29 10:43:33 -0600

    Se agregaron las funcionalidades del modelo de tarea -Angel Mendez- -Edson Pereira-

--- src/tareas/tarea.routes.js
+++ src/tareas/tarea.routes.js
@@ -1,32 +1,34 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { tareasGet, tareasPost } from "./tarea.controller.js";
+import { tareasDelete, tareasGet, tareasPost, tareasPut } from "./tarea.controller.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 
 const router = Router()
 
-router.get( "/verTareas", tareasGet );
+router.get("/verTareas", tareasGet);
 
 router.post(
     "/agregar", [
-    check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
-    check( "description", "La descripcion es obligatoria" ).not().isEmpty(),
-    check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
-    check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
-    check( "name", "El nombre es obligatorio" ).not().isEmpty(),
-    check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
+    check("nombreTarea", "El nombre de la tarea es obligatoria").not().isEmpty(),
+    check("descripcion", "La descripcion es obligatoria").not().isEmpty(),
+    check("fechaDeInicio", "La fecha de inicio es obligatoria").not().isEmpty(),
+    check("fechaDeCierre", "La fecha de cierre es obligatoria").not().isEmpty(),
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("lastName", "El apellido es obligatorio").not().isEmpty(),
     validarCampos
 ],
     tareasPost
-);
+),
 
 router.put( '/actualizar/:id', [check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
-check( "description", "La descripcion es obligatoria" ).not().isEmpty(),
+check( "descripcion", "La descripcion es obligatoria" ).not().isEmpty(),
 check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
 check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
 check( "estado", "El estado es obligatorio" ).not().isEmpty(),
 check( "name", "El nombre es obligatorio" ).not().isEmpty(),
 check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
     validarCampos], tareasPut );
+
+router.delete('/delete/:id', validarCampos, tareasDelete)
 
 export default router;
