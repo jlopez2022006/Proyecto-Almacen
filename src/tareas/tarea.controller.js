0fea1bbd141a56180d999dee42a39219e4097756
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-05-03 04:19:23 -0600

    did things

--- src/tareas/tarea.controller.js
+++ src/tareas/tarea.controller.js
@@ -1,45 +1,45 @@
 import Tarea from "../tareas/tarea.model.js"
 import bcrypts from "bcryptjs"
 
-export const tareasGet = async (req, res) => {
+export const tareasGet = async ( req, res ) => {
     try {
         const tareas = await Tarea.find();
 
-        res.status(200).json(tareas);
-    } catch (error) {
-        console.error("Error al obtener las tareas:", error);
-        res.status(500).json({ error: 'Error al obtener las tareas' });
+        res.status( 200 ).json( tareas );
+    } catch ( error ) {
+        console.error( "Error al obtener las tareas:", error );
+        res.status( 500 ).json( { error: 'Error al obtener las tareas' } );
     }
 };
 
-export const tareasPost = async (req, res) => {
+export const tareasPost = async ( req, res ) => {
     try {
         const { nombreTarea, descripcion, fechaDeInicio, fechaDeCierre, name, lastName } = req.body;
 
-        const nuevaTarea = new Tarea({
+        const nuevaTarea = new Tarea( {
             nombreTarea,
             descripcion,
             fechaDeInicio,
             fechaDeCierre,
             name,
             lastName
-        });
+        } );
 
         await nuevaTarea.save();
 
-        res.status(201).json(nuevaTarea);
-    } catch (error) {
-        console.error("Error al agregar la tarea:", error);
-        res.status(500).json({ error: 'Error al agregar la tarea' });
+        res.status( 201 ).json( nuevaTarea );
+    } catch ( error ) {
+        console.error( "Error al agregar la tarea:", error );
+        res.status( 500 ).json( { error: 'Error al agregar la tarea' } );
     }
 };
 
-export const tareasPut = async (req, res) => {
+export const tareasPut = async ( req, res ) => {
     try {
         const { id } = req.params;
         const { nombreTarea, descripcion, fechaDeInicio, fechaDeCierre, estado, name, lastName } = req.body;
 
-        const tareaActualizada = await Tarea.findByIdAndUpdate(id, {
+        const tareaActualizada = await Tarea.findByIdAndUpdate( id, {
             nombreTarea,
             descripcion,
             fechaDeInicio,
@@ -47,40 +47,43 @@ export const tareasPut = async (req, res) => {
             estado,
             name,
             lastName
-        });
+        }, { new: true } );
 
         await tareaActualizada.save();
 
-        res.status(200).json(tareaActualizada);
-    } catch (error) {
-        console.error("Error al actualizar la tarea:", error);
-        res.status(500).json({ error: 'Error al actualizar la tarea' });
+        res.status( 200 ).json( tareaActualizada );
+    } catch ( error ) {
+        console.error( "Error al actualizar la tarea:", error );
+        res.status( 500 ).json( { error: 'Error al actualizar la tarea' } );
     }
 }
 
-export const tareasDelete = async (req, res) => {
+export const tareasDelete = async ( req, res ) => {
     const { id } = req.params;
-    await Tarea.findByIdAndDelete(id);
+    const deletedTask = await Tarea.findByIdAndDelete( { _id: id } );
 
-    res.status(200).json({ msg: 'Se elimino la tarea asignada' })
+    res.status( 200 ).json( { msg: 'Se elimino la tarea asignada', Task: deletedTask } )
 }
 
-export const putEstado = async (req, res) => {
+export const putEstado = async ( req, res ) => {
     try {
         const { id } = req.params;
-        const { estado } = req.body;
+        const tarea = await Tarea.findById( id );
+        const estadoTarea = () => {
+            return tarea.estado
+        }
+        const tareaActualizada = await Tarea.findByIdAndUpdate( id, {
+            estado: !estadoTarea()
+        }, { new: true } );
 
-        const tareaActualizada = await Tarea.findByIdAndUpdate(id, {
-            estado
-        }, { new: true });
-
-        if (!tareaActualizada) {
-            return res.status(404).json({ error: 'Tarea no encontrada' });
+        await tareaActualizada.save();
+        if ( !tareaActualizada ) {
+            return res.status( 404 ).json( { error: 'Tarea no encontrada' } );
         }
 
-        res.status(200).json(tareaActualizada);
-    } catch (error) {
-        console.error("Error al actualizar el estado de la tarea:", error);
-        res.status(500).json({ error: 'Error al actualizar el estado de la tarea' });
+        res.status( 200 ).json( { tareaActualizada } );
+    } catch ( error ) {
+        console.error( "Error al actualizar el estado de la tarea:", error );
+        res.status( 500 ).json( { error: 'Error al actualizar el estado de la tarea' } );
     }
 };
--- src/tareas/tarea.routes.js
+++ src/tareas/tarea.routes.js
@@ -5,32 +5,31 @@ import { validarCampos } from "../middlewares/validar-campos.js";
 
 const router = Router()
 
-router.get("/verTareas", tareasGet);
+router.get( "/verTareas", tareasGet );
 
 router.post(
     "/agregar", [
-    check("nombreTarea", "El nombre de la tarea es obligatoria").not().isEmpty(),
-    check("descripcion", "La descripcion es obligatoria").not().isEmpty(),
-    check("fechaDeInicio", "La fecha de inicio es obligatoria").not().isEmpty(),
-    check("fechaDeCierre", "La fecha de cierre es obligatoria").not().isEmpty(),
-    check("name", "El nombre es obligatorio").not().isEmpty(),
-    check("lastName", "El apellido es obligatorio").not().isEmpty(),
+    check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
+    check( "descripcion", "La descripcion es obligatoria" ).not().isEmpty(),
+    check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
+    check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
+    check( "name", "El nombre es obligatorio" ).not().isEmpty(),
+    check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
     validarCampos
 ],
     tareasPost
 ),
 
-router.put( '/actualizar/:id', [check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
-check( "descripcion", "La descripcion es obligatoria" ).not().isEmpty(),
-check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
-check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
-check( "estado", "El estado es obligatorio" ).not().isEmpty(),
-check( "name", "El nombre es obligatorio" ).not().isEmpty(),
-check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
-    validarCampos], tareasPut );
+    router.put( '/actualizar/:id', [check( "nombreTarea", "El nombre de la tarea es obligatoria" ).not().isEmpty(),
+    check( "descripcion", "La descripcion es obligatoria" ).not().isEmpty(),
+    check( "fechaDeInicio", "La fecha de inicio es obligatoria" ).not().isEmpty(),
+    check( "fechaDeCierre", "La fecha de cierre es obligatoria" ).not().isEmpty(),
+    check( "name", "El nombre es obligatorio" ).not().isEmpty(),
+    check( "lastName", "El apellido es obligatorio" ).not().isEmpty(),
+        validarCampos], tareasPut );
 
-router.put('/estado/:id', validarCampos, putEstado);    
+router.put( '/estado/:id', validarCampos, putEstado );
 
-router.delete('/delete/:id', validarCampos, tareasDelete)
+router.delete( '/delete/:id', validarCampos, tareasDelete )
 
 export default router;
